7da99fdd0a0f3b7df26be4f3d78421d017069f3f
chichuot96 <chi96nm@gmail.com>
2020-11-27 14:08:30 +0700

    them edit

---
+++ Sem3_backend/Scripts/getCkEditorForEdit.js
@@ -0,0 +1,8 @@
+﻿window.onload = function () {
+    editor = CKEDITOR.replace('Content');
+    editor.on('change', function (evt) {
+        var content = document.querySelector('input[name="Content"]');
+        content.value = evt.editor.getData();
+        console.log(content.value);
+    });
+}
